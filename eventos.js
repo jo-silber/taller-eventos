5dcbb074993a4f7fb5adcc8065d4d53110528ced
Josefina <josesilbermann@gmail.com>
2024-08-13 13:32:57 -0300

    No incluí el js
    Me olvide de pushear el archivo js en la main y no aparece en github

--- eventos.js
+++ eventos.js
@@ -2,7 +2,3 @@ document.getElementById("divBoton").addEventListener("click", () => {
     alert("Hola! Soy el div");
 });
 
-document.getElementById("botonSaludar").addEventListener("click", (event) =>{
-    event.stopPropagation();
-    alert("Hola!");
-})
