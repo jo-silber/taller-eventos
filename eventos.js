ccd73b54e106e947c75374d836c4c69e1a35029b
Josefina <josesilbermann@gmail.com>
2024-08-13 13:40:15 -0300

    Agregué la solucion a la propagacion

--- eventos.js
+++ eventos.js
@@ -1,4 +1,10 @@
 document.getElementById("divBoton").addEventListener("click", () => {
-    alert("Hola! Soy el div");
+  alert("Hola! Soy el div");
 });
+
+document.getElementById("botonSaludar").addEventListener("click", (event) =>{
+  event.stopPropagation();
+  alert("Hola!");
+});
+
 
